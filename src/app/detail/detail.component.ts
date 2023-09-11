8643563e407582d3bc3273530a81e288e6c155db
Sanjd <jojoloiseaumusique@gmail.com>
2023-09-11 20:15:01 +0200

    premier jet

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  public name = '';
+  public list: string[] = [];
+  public nouvelObjet: string = '';
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.name = params.get('id') ?? '';
+      this.getTheList();
+    });
+  }
+
+  comeBackHome() {
+    this.router.navigate(['/']);
+  }
+
+  private getTheList(): void {
+    this.list = JSON.parse(localStorage.getItem(this.name) ?? '');
+  }
+
+  public addInList(): void {
+    if(!!this.nouvelObjet && this.nouvelObjet.length >0){
+      this.list = [...this.list, this.nouvelObjet];
+      localStorage.setItem(this.name, JSON.stringify(this.list));
+      this.getTheList();
+    }
   }
 
 }
